1d8235c5e617cbda3ac2e083a1dcfc13688bbbdd
Lai-Ting <tim19980304@gmail.com>
2024-03-14 14:37:35 +0800

    新增關於我們,保健品知識

---
+++ Long_Term_Care/Models/health_info.cs
@@ -0,0 +1,10 @@
+﻿namespace Long_Term_Care.Models
+{
+	public class health_info_text
+	{
+		public int info_Id { get; set; }
+		public string? info_img { get; set; }
+		public string? info_title { get; set; }
+		public string? info_context { get; set; }
+	}
+}
